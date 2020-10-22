e8776daf1d8afb589ad2cc7875d781e380b921db
ice <50304390+ice-10@users.noreply.github.com>
2020-10-22 10:58:38 -0500

    Add files via upload

--- ValorantStats.py
+++ ValorantStats.py
@@ -2,7 +2,7 @@ import discord
 import datetime
 from discord.ext import commands
 from discord.utils import get
-
+import json
 from dhooks import Webhook, Embed
 
 
@@ -107,5 +107,36 @@ async def stats(ctx):
     hook.send(embed=statistics_embed, username='ValorantStats',avatar_url='https://cdn.discordapp.com/avatars/764330534556925973/b796c759a2322eb3a5837d07ee102d56.png?size=128')
 
 
+@client.command()
+async def trends(ctx):
 
-client.run(#insert token here)
+    match_description = """
+    ***Trends over the past act***
+
+    **Kill Death Ratio**:
+    **Accuracy**:
+    **Headshot Accuracy**:
+
+
+
+    """
+
+
+    trends_embed = Embed(
+        title = 'Trend History',
+        timestamp = 'now',
+        color = 5000,
+        description = match_description,
+
+
+    )
+    trends_embed.set_image('https://quickchart.io/chart?bkg=transparent&c=%7B%0A%20%20type%3A%20%27line%27%2C%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%2F%2F%20Show%20a%20bar%20chart%0A%20%20data%3A%20%7B%0A%20%20%20%20labels%3A%20%5B%27Act%201%27%2C%20%27Act%202%27%2C%20%27Act%203%27%5D%2C%20%20%20%2F%2F%20Set%20X-axis%20labels%0A%20%20%20%20yAxisID%3A%20100%2C%0A%20%20%20%20backgroundColor%3A%20%27rgba(255%2C255%2C0%2C1)%27%2C%0A%20%20%20%20%0A%20%20%20%20datasets%3A%20%5B%7B%0A%20%20%20%20%20%20label%3A%20%27Accuracy%27%2C%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%2F%2F%20Create%20the%20%27Users%27%20dataset%0A%20%20%20%20%20%20data%3A%20%5B32%2C%2040%2C%2043%5D%2C%20%20%0A%20%20%20%20%20%20%2F%2F%20Add%20data%20to%20the%20chart%0A%20%20%20%20%20%20borderColor%3A%20%27rgba(255%2C0%2C0%2C1)%27%2C%0A%20%20%20%20%20%20pointBorderColor%3A%20%27rgba(255%2C255%2C0%2C)%27%2C%0A%20%20%20%20%20%20pointBackgroundColor%3A%20%27rgba(0%2C0%2C0%2C1)%27%2C%0A%20%20%20%20%20%20fontColor%20%3A%27black%27%2C%0A%20%20%20%20%20%20%0A%20%20%20%20%20%20fill%3A%20false%2C%0A%20%20%20%20%20%20%0A%20%20%20%20%7D%5D%0A%20%20%20%20%0A%20%20%7D%0A%7D')
+
+
+    hook.send(embed=trends_embed, username='ValorantStats',avatar_url='https://cdn.discordapp.com/avatars/764330534556925973/b796c759a2322eb3a5837d07ee102d56.png?size=128')
+
+
+
+
+
+client.run('Insert Token Here')
